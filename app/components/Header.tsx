ff6fc5c561e9ea91044db9bcedb48bb5cccc9fc5
ngm-noahmifsud <nmifsud.ngm@gmail.com>
2025-08-26 22:29:07 +1000

    feat(navbar): update visuals and add hamburger menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,12 +6,12 @@ type HeaderProps = {
 
 export default function Header({ title }: HeaderProps) {
   return (
-    <header className='flex flex-col h-20'>
-        <div className='flex-1 flex bg-blue-200 justify-center items-center'>
+    <header className='flex flex-col h-20 bg-white dark:bg-black text-black dark:text-white'>
+        <div className='flex-1 flex justify-center items-center'>
             <h1 className=''>{title}</h1>
             <p className='absolute top-0 left-0 p-1 font-bold'>22068917</p>
         </div>
-        <div className='flex-1 bg-green-200'>
+        <div className='flex-1'>
             <Navbar />
         </div>
     </header>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,43 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
+
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className='bg-green-500 h-full dark:bg-neutral-900'>
-        Nav
+    <nav className='h-full'>
+      <div className={`bg-white dark:bg-black fixed inset-0 z-50 transform transition-transform duration-500 ease-out ${menuOpen ? "translate-y-0" : "-translate-y-full"}`}>
+        <div className='m-10 h-[calc(100%-5rem)]'>
+          <div className='flex justify-between'>
+            <h1 className='text-4xl font-bold'>MENU</h1>
+            <button onClick={() => setMenuOpen(!menuOpen)} className='hover:cursor-pointer'><IoIosCloseCircle className='text-4xl'/></button>
+          </div>
+          <ul className='mt-5 [&>li]:text-2xl [&>li]:border-b [&>li]:border-black [&>li]:dark:border-white'>
+            <li><a href='/'>Homepage</a></li>
+            <li><a href='/about'>About</a></li>
+            <li><a href='#'>Escape Room</a></li>
+            <li><a href='#'>Coding Races</a></li>
+            <li><a href='#'>Court Room</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <ul className='flex justify-center [&>li]:p-2 [&>li]:border-r [&>li]:border-black [&>li]:dark:border-white'>
+          <li><a href='/'>Homepage</a></li>
+          <li><a href='/about'>About</a></li>
+          <li><a href='#'>Escape Room</a></li>
+          <li><a href='#'>Coding Races</a></li>
+          <li><a href='#'>Court Room</a></li>
+        </ul>
+        <button className='aspect-square mr-2 flex items-center hover:cursor-pointer' onClick={() => setMenuOpen(!menuOpen)}>
+          <RxHamburgerMenu className='text-2xl'/>
+        </button>
+
+      </div>
     </nav>
   )
 }
